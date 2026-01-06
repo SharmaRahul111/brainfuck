5e96e324c457510d3bdbc824031ef1e0be86383d
Pramendra Sharma ( Rahul ) <74184362+SharmaRahul111@users.noreply.github.com>
2026-01-06 16:31:05 +0545

    implemented the 6 commands except for loops []

--- src/main.c
+++ src/main.c
@@ -3,6 +3,35 @@
 int main(){
     unsigned char tape[30000];
     unsigned short int pointer = 0;
-
+    char command[1000] = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++";
+    unsigned short int code_pointer = 0;
+    while(command[code_pointer]!='\0'){
+        switch(command[code_pointer]){
+            case '+':
+                tape[pointer]++;
+                break;
+            case '-':
+                tape[pointer]--;
+                break;
+            case '>':
+                pointer++;
+                break;
+            case '<':
+                pointer--;
+                break;
+            case '.':
+                printf("%c", tape[pointer]);
+            case ',':
+                scanf("%d", &tape[pointer]);
+            case '[':
+                // will implement later
+                break;
+            case ']':
+                // later
+                break;
+            default:
+                ; // ignore
+        }
+    }
     return 0;
 }
